833bf438438153ff3ea17752d8b32795e5fa6400
lytex <julianlpc15@gmail.com>
2020-08-07 15:47:52 +0200

    wrap everything into function add_task

--- add_task.py
+++ add_task.py
@@ -10,17 +10,9 @@ def generate_id():
     return (''.join(random.choice(string.ascii_letters+string.digits+'_-')
                         for _ in range(random.choice(range(9,11)))))
 
-with open('1.json') as f:
-    j = json.loads(f.read())
-
-## TODO Buscar los ids en 3.json y ponerlo en todos los sitios en los que está
-
 id = generate_id()
 project_id = 'VqTvrMl6c'
-j['task']['ids'].append(id)
-j['project']['entities'][project_id]['taskIds'].append(id)
-
-j['task']['entities'][id] = {
+task = {
     'id': id,
     'projectId': project_id,
     'subTaskIds': [],
@@ -29,8 +21,8 @@ j['task']['entities'][id] = {
     'timeEstimate': 0,
     'isDone': False,
     'doneOn': None,
-    'title': 'Tarea de prueba',
-    'notes': '',
+    'title': 'Un nuevno nombre',
+    'notes': '\nEsto no tiene mucho sentido pero bueno\n**Negrita**\n_Cursiva_',
     'tagIds': [],
     'parentId': None,
     'reminderId': None,
@@ -45,6 +37,20 @@ j['task']['entities'][id] = {
     'issueLastUpdated': None,
     'issueWasUpdated': None
 }
+
+def add_task(j, task, id, project_id):
+
+    j['task']['ids'].append(id)
+    j['project']['entities'][project_id]['taskIds'].append(id)
+
+    j['task']['entities'][id] = task
+    return j
+
+
+with open('1.json') as f:
+    j = json.loads(f.read())
+
+j = add_task(j, task, id, project_id)
 
 with open('2.json', 'w') as f:
     json.dump(j, f)
